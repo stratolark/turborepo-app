617366a603be302d75572090a2d931b2f975e887
stratolark <69923983+stratolark@users.noreply.github.com>
2022-10-07 22:16:02 -0300

    feat: add redis session to server.

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -3,19 +3,40 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import actuator from "express-actuator";
+import session from "express-session";
+import redis from "connect-redis";
+import { redisClient } from "./config/redis";
+import { SESSION_SECRET } from "./config/env";
+
+const RedisStore = redis(session);
 
 export const createServer = () => {
   const app = express();
-  app
-    .disable("x-powered-by")
-    .use(morgan("dev"))
-    .use(urlencoded({ extended: true }))
-    .use(json())
-    .use(cors())
-    .use(actuator())
-    .get("/message/:name", (req, res) => {
-      return res.json({ message: `hello ${req.params.name}` });
-    });
+
+  // middleware
+  app.disable("x-powered-by");
+  app.use(morgan("dev"));
+  app.use(urlencoded({ extended: true }));
+  app.use(json());
+  app.use(cors());
+
+  // Sessions with Redis
+  app.use(
+    session({
+      store: new RedisStore({ client: redisClient }),
+      saveUninitialized: false,
+      secret: SESSION_SECRET as string,
+      resave: false,
+    })
+  );
+
+  // Health checks
+  app.use(actuator());
+
+  // Routes
+  app.get("/message/:name", (req, res) => {
+    return res.json({ message: `hello ${req.params.name}` });
+  });
 
   return app;
 };
